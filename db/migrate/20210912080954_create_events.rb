2ee9f52b12317f639581a6b07541214cefecdcaa
38takumi <saitout485@gmail.com>
2021-09-16 12:03:40 +0000

    [fix]マイグレーションファイル修正

--- db/migrate/20210912080954_create_events.rb
+++ db/migrate/20210912080954_create_events.rb
@@ -3,8 +3,8 @@ class CreateEvents < ActiveRecord::Migration[5.2]
     create_table :events do |t|
       t.string :event_title,        null: false, default: ""
       t.text :event_description
-      t.datetime :event_start,      null: false, default: ""
-      t.datetime :event_end,        null: false, default: ""
+      t.datetime :event_start,      null: false
+      t.datetime :event_end,        null: false
 
       t.integer :customer_id,       null: false
 
